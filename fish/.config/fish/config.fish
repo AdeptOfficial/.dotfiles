671c555de50be5232bb76071e0f4cbe816bd6b1e
AdepT <Leland.d.moy@gmail.com>
2025-12-14 02:47:42 -0600

    Initial commit: Omarchy dotfiles with stow
    - Hyprland configs (all .conf files)
    - Fish config
    - Using GNU stow for symlink management

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# Start SSH agent if not already running
+if status is-interactive
+    keychain --quiet --eval id_ed25519 | source
+end
